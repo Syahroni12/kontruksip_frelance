c003c9ee56bb2e6a16a0aedb1c0c54c5c8cb02f4
Syahroni12 <ronixria@gmail.com>
2024-09-25 22:49:55 +0700

    templating_landingpage

---
+++ resources/views/templating_landing/footer.blade.php
@@ -0,0 +1,9 @@
+ <!-- copyright section end -->
+      <!-- Javascript files-->
+      <script src="{{ asset('landing_page/js/jquery.min.js') }}"></script>
+      <script src="{{ asset('landing_page/js/popper.min.js') }}"></script>
+      <script src="{{ asset('landing_page/js/bootstrap.bundle.min.js') }}"></script>
+      <script src="{{ asset('landing_page/js/jquery-3.0.0.min.js') }}"></script>
+      <script src="{{ asset('landing_page/js/plugin.js') }}"></script>
+   </body>
+</html>
---
+++ resources/views/templating_landing/header.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Builder Max</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" type="text/css" href="{{ asset('landing_page/css/bootstrap.min.css') }}">
+      <!-- style css -->
+      <link rel="stylesheet" type="text/css" href="{{ asset('landing_page/css/style.css') }}">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="{{ asset('landing_page/images/fevicon.png') }}" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="{{ asset('landing_page/css/jquery.mCustomScrollbar.min.css') }}">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <!-- fonts -->
+      <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;800&family=Sen:wght@400;700;800&display=swap" rel="stylesheet">
+   </head>
